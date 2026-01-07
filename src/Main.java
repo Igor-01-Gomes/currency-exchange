203cf534d4b6b88c604724f075199815a5782815
Daniel Lyytikäinen <daniel.lyytikainen@yh.nackademin.se>
2026-01-07 13:29:59 +0100

    Dropdown menu changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import javax.swing.SwingUtilities;
 
 void main() {
-    // CURRENCY EXCHANGE RATE ARE FROM 7/1 - 2026
+    // CURRENCY EXCHANGE RATES ARE FROM 7/1 - 2026
     SwingUtilities.invokeLater(() -> {
         ConverterGUI gui = new ConverterGUI();
         gui.setVisible(true);
